69474531d4d1d361df463df364833136f0ffe48f
ezehmark <ezehmark@gmail.com>
2026-03-15 15:32:32 +0100

    made new changes

--- frontend/src/app/dashboard/agents/page.tsx
+++ frontend/src/app/dashboard/agents/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   MoreVertical,
   Search as SearchIcon,
@@ -125,7 +125,9 @@ export default function VoiceAgentsView() {
     }
   };
 
-
+useEffect(()=>{
+  fetchAgents()
+},[])
   const filteredAgents =() => {
     let arr = [...agents];
     if (searchQuery) {
